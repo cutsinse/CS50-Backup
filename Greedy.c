525111438d9d28916f1107a71028427d89695c5a
cutsinse <34425610+cutsinse@users.noreply.github.com>
2017-12-29 04:33:20 +0100

    Getting Started
    -get float from user, identify as "d"
    -convert float d to int c (cents)

---
+++ Greedy.c
@@ -0,0 +1,25 @@
+/*Less comfortable: the goal of this program is to show the least number of coins that can be dispirsed in order to give change */
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    float d;
+    do
+    {
+    d = get_float("Change:");
+    }
+    while (d < 0);
+    int c = d *100;
+
+    int ct = 0;
+
+}
+
+
+
+void quarter(int ct)
+{
+
+}
